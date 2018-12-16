6adb0d54f144228a3289cb6752758e3031a62595
StrikerRocker <thariksherief1@gmail.com>
2018-12-16 13:03:28 +0530

    Initial commit

--- src/main/java/io/github/strikerrocker/brokenxp/BrokenXP.java
+++ src/main/java/io/github/strikerrocker/brokenxp/BrokenXP.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package io.github.strikerrocker.brokenxp;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class BrokenXP implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
